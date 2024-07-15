a4de7203f1edd6442f1408564217b0eb458cfa22
Benjamin <root@baryon.it>
2024-07-15 14:21:22 -0700

    cleaned aaudio code

--- AAudio-write.c
+++ AAudio-write.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <aaudio/AAudio.h>
-#include <android/api-level.h>
-
-// #define __ANDROID_API__ 30
 
 #define BUFFER_SIZE 2048
+
+AAudioStreamBuilder *builder;
+AAudioStream *stream;
+
+void halt(char ret){
+	if(stream)  AAudioStream_close(stream);
+	if(builder) AAudioStreamBuilder_delete(builder);
+	exit(ret);
+}
+
+void check(aaudio_result_t result,const char* action,const char* file,const int line){
+	if (result != AAUDIO_OK){
+		if(action){
+			fprintf(stderr,"Error '%s' in file '%s' at line %d.",AAudio_convertResultToText(result),file,line);
+		}else{
+			fprintf(stderr,"Failed to %s in file '%s' at line %d. %s",action,file,line,AAudio_convertResultToText(result));
+		}
+		halt(1);
+	}
+}
+
+#define AACKM(x,m) check(x,m,__FILE__,__LINE__)
+#define AACK(x) AACKM(x,NULL)
 
 void errorCallback(AAudioStream *stream, void *userData, aaudio_result_t error) {
     fprintf(stderr, "Error: %s\n", AAudio_convertResultToText(error));
@@ -18,15 +38,9 @@ int fillBufferWithPCMData(AAudioStream *stream, void *userData, void *audioData,
 }
 
 int main() {
-    AAudioStreamBuilder *builder;
-    AAudioStream *stream;
     aaudio_result_t result;
 
-    result = AAudio_createStreamBuilder(&builder);
-    if (result != AAUDIO_OK) {
-        fprintf(stderr, "Failed to create stream builder: %s\n", AAudio_convertResultToText(result));
-        return EXIT_FAILURE;
-    }
+	 AACKM(AAudio_createStreamBuilder(&builder),"create stream builder");
 
     AAudioStreamBuilder_setFormat(builder, AAUDIO_FORMAT_PCM_I16);
     AAudioStreamBuilder_setChannelCount(builder, 2);
@@ -36,32 +50,11 @@ int main() {
     AAudioStreamBuilder_setFramesPerDataCallback(builder, BUFFER_SIZE / (sizeof(int16_t) * 2));
     AAudioStreamBuilder_setDataCallback(builder, fillBufferWithPCMData, NULL);
 
-    result = AAudioStreamBuilder_openStream(builder, &stream);
-    if (result != AAUDIO_OK) {
-        fprintf(stderr, "Failed to open stream: %s\n", AAudio_convertResultToText(result));
-        AAudioStreamBuilder_delete(builder);
-        return EXIT_FAILURE;
-    }
-
-    result = AAudioStream_requestStart(stream);
-    if (result != AAUDIO_OK) {
-        fprintf(stderr, "Failed to start stream: %s\n", AAudio_convertResultToText(result));
-        AAudioStream_close(stream);
-        AAudioStreamBuilder_delete(builder);
-        return EXIT_FAILURE;
-    }
+    AACKM(AAudioStreamBuilder_openStream(builder, &stream),"open stream");
+    AACKM(AAudioStream_requestStart(stream),"start stream");
 
     while (1) {
         // Keep the stream running
     }
-
-    AAudioStream_close(stream);
-    AAudioStreamBuilder_delete(builder);
-
-    return EXIT_SUCCESS;
+	 halt(0);
 }
-
-
-//AAudioStream_dataCallback (AAudioStream *, void *, void *, int32_t)' 
-//(aka 'int (*(struct AAudioStreamStruct *, void *, void *, int))(struct AAudioStreamStruct *, void *, void *, int)') to parameter of type 
-//'AAudioStream_dataCallback' (aka 'int (*)(struct AAudioStreamStruct *, void *, void *, int)'
