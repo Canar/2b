8ada24b67a7ae5405987af1961a4b8ba21c6b3eb
Benjamin Cook <root@baryon.it>
2025-04-19 19:43:57 -0700

    missed a new file

---
+++ args.c
@@ -0,0 +1,40 @@
+#include <getopt.h>
+/* NOTE: requires halt() defined before including. */
+
+void usage(const char *prog_name) {
+	fprintf(stderr, "Usage: %s [options]\n", prog_name);
+	fprintf(stderr, "Reads raw PCM audio data from stdin and plays it via %s.\n",API);
+	fprintf(stderr, "\nOptions:\n");
+	fprintf(stderr, "  -r, --rate <rate>       Set the sample rate in Hz (default: 44100)\n");
+	fprintf(stderr, "  -c, --channels <chans>  Set the number of channels (default: 1)\n");
+	fprintf(stderr, "  -h, --help              Display this help message\n");
+}
+
+void handle_args(int argc,char**argv,int*chan,int*rate){
+	static struct option long_options[] = {
+		{"rate",     required_argument, 0, 'r'},
+		{"channels", required_argument, 0, 'c'},
+		{"help",     no_argument,       0, 'h'},
+	{0,0,0,0}};
+
+	int opt;
+	int index = 0;
+	char *endptr;
+
+	while ((opt = getopt_long(argc, argv, "r:c:h", long_options, &index)) != -1)
+	switch (opt) {
+	case 'r':
+		(*rate) = (int)strtol(optarg, &endptr, 10);
+		break;
+	case 'c':
+		(*chan) = (int)strtol(optarg, &endptr, 10);
+		break;
+	case 'h':
+	case '?':
+	default:
+		usage(argv[0]);
+		halt(0);
+	}
+}
+
+
