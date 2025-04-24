ed8a50d7390c5f16c123e81540f1142fe1fe4ffa
Benjamin Cook <root@baryon.it>
2025-04-23 19:56:02 -0700

    renamed some vars, cleaned some code

--- OpenSL-ES-write.c
+++ OpenSL-ES-write.c
@@ -4,31 +4,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-SLObjectItf engineObject=NULL, outputMixObject=NULL, playerObject=NULL;
-SLEngineItf engineEngine = NULL;
+SLObjectItf sles=NULL, outmix=NULL, playerObject=NULL;
+SLEngineItf engine = NULL;
 SLPlayItf playerPlay = NULL;
 SLAndroidSimpleBufferQueueItf playerBufferQueue = NULL;
 
 #define BUFFER_SIZE 8192
-short buffer[BUFFER_SIZE];
+char buffer[BUFFER_SIZE];
 
 volatile int eof=0;
 #define CALL(object,method,...) (*(object))->method((object),##__VA_ARGS__)
 #define DESTROY(x) if(x!=NULL) CALL(x,Destroy)
 
-void playerCallback(SLAndroidSimpleBufferQueueItf bq, void *context) {
-	size_t bytes_read = fread(buffer, sizeof(short), BUFFER_SIZE, stdin);
+void fill_buffer(SLAndroidSimpleBufferQueueItf bq, void *context) {
+	size_t bytes_read = fread(buffer,1,BUFFER_SIZE,stdin);
 	eof=bytes_read<=0;
-	CALL(bq,Enqueue,buffer,bytes_read * sizeof(short));
+	CALL(bq,Enqueue,buffer,bytes_read);
 }
 
 int main(int argc, char *argv[]) {
-	slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
-	CALL(engineObject,Realize,SL_BOOLEAN_FALSE);
-	CALL(engineObject,GetInterface,SL_IID_ENGINE,&engineEngine);
-	CALL(engineEngine,CreateOutputMix,&outputMixObject,0,NULL,NULL);
-	CALL(outputMixObject,Realize,SL_BOOLEAN_FALSE);
-	SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+	slCreateEngine(&sles, 0, NULL, 0, NULL, NULL);
+	CALL(sles,Realize,SL_BOOLEAN_FALSE);
+	CALL(sles,GetInterface,SL_IID_ENGINE,&engine);
+	CALL(engine,CreateOutputMix,&outmix,0,NULL,NULL);
+	CALL(outmix,Realize,SL_BOOLEAN_FALSE);
 	/*
 	SLDataFormat_PCM format_pcm = {
 		SL_DATAFORMAT_PCM,
@@ -44,25 +43,25 @@ int main(int argc, char *argv[]) {
 		SL_ANDROID_DATAFORMAT_PCM_EX, 1, SL_SAMPLINGRATE_44_1, 32, 32,
 		SL_SPEAKER_FRONT_LEFT, SL_BYTEORDER_LITTLEENDIAN, SL_ANDROID_PCM_REPRESENTATION_FLOAT
 	};
+	SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
 	SLDataSource audioSrc = {&loc_bufq, &format_pcm};
-	SLDataLocator_OutputMix loc_outmix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
+	SLDataLocator_OutputMix loc_outmix = {SL_DATALOCATOR_OUTPUTMIX, outmix};
 	SLDataSink audioSnk = {&loc_outmix, NULL};
-
 	const SLInterfaceID ids[] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
 	const SLboolean req[] = {SL_BOOLEAN_TRUE};
-	CALL(engineEngine,CreateAudioPlayer, &playerObject, &audioSrc, &audioSnk, 1, ids, req);
+	CALL(engine,CreateAudioPlayer, &playerObject, &audioSrc, &audioSnk, 1, ids, req);
 	CALL(playerObject,Realize,SL_BOOLEAN_FALSE);
 	CALL(playerObject,GetInterface, SL_IID_PLAY, &playerPlay);
 	CALL(playerObject,GetInterface, SL_IID_ANDROIDSIMPLEBUFFERQUEUE, &playerBufferQueue);
-	CALL(playerBufferQueue,RegisterCallback, playerCallback, NULL);
+	CALL(playerBufferQueue,RegisterCallback,fill_buffer, NULL);
 	CALL(playerPlay,SetPlayState, SL_PLAYSTATE_PLAYING);
-	playerCallback(playerBufferQueue, NULL);
+	fill_buffer(playerBufferQueue, NULL);
 	
 	while (!eof) usleep(1000 * 1000 / 24); //24Hz sleep. Cinema!
 	
 	DESTROY(playerObject);
-	DESTROY(outputMixObject);
-	DESTROY(engineObject);
+	DESTROY(outmix);
+	DESTROY(sles);
 
 	return 0;
 }
